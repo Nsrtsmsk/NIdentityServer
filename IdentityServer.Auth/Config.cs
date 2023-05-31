31ba4cf6ca8eaabff316da9c1b21ff1b1ed5feb9
Nusret <Nusret@NUSRET>
2023-05-31 21:38:34 +0300

    Add project files.

---
+++ IdentityServer.Auth/Config.cs
@@ -0,0 +1,42 @@
+﻿using IdentityServer4.Models;
+
+namespace IdentityServer.Auth
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> GetAPIResource()
+        {
+            return new List<ApiResource>()
+            {
+                new ApiResource("resource_api1")
+                { Scopes =
+                    {
+                        "api1.read",
+                        "api1.write",
+                        "api1.update"
+                    }
+                },
+                new ApiResource("resource_api2") 
+                { Scopes =
+                    {
+                        "api2.read",
+                        "api2.write",
+                        "api2.update"
+                    }
+                }
+            };
+        }
+        public static IEnumerable<ApiScope> GetAPIScopes()
+        {
+            return new List<ApiScope>() 
+            { new ApiScope("api1.read", "Read permission for api1"),
+              new ApiScope("api1.write","Write permission for api1"),
+              new ApiScope("api1.update","Update permission for api1"),
+              new ApiScope("api2.read", "Read permission for api2"),
+              new ApiScope("api2.write","Write permission for api2"),
+              new ApiScope("api2.update","Update permission for api2")
+            };
+        }
+
+    }
+}
