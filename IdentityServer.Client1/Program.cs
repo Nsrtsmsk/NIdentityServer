1031d5685f1ac0c1389509bfd7f8dfdeaff1a99b
Nusret Şimşek <nsimsek@mngkargo.com.tr>
2023-06-16 16:35:12 +0300

    hiçbişey

--- IdentityServer.Client1/Program.cs
+++ IdentityServer.Client1/Program.cs
@@ -4,8 +4,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddAuthentication(opts =>
 {
-    opts.DefaultScheme = "Cookies";opts.DefaultScheme = "oidc";
-}).AddCookie("Cookies").AddOpenIdConnect("oidc", opts =>
+    opts.DefaultScheme = "Cookies";
+    opts.DefaultScheme = "oidc";
+})
+    .AddCookie("Cookies")
+    .AddOpenIdConnect("oidc", opts =>
 {
     opts.SignInScheme = "Cookies";
     opts.Authority = "https://localhost:44383";
@@ -15,11 +18,9 @@ builder.Services.AddAuthentication(opts =>
 });
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
