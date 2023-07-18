4a6acea60f9fd66409d5e3760e03e57c36f34d6d
Nusret Şimşek <nsimsek@mngkargo.com.tr>
2023-07-18 16:57:59 +0300

    Custom Claim Eklendi. Şehir ve Ülke bilgileri. Client1-mvc

--- IdentityServer.Auth/Config.cs
+++ IdentityServer.Auth/Config.cs
@@ -5,131 +5,140 @@ using System.Security.Claims;
 
 namespace IdentityServer.Auth
 {
-	public static class Config
-	{
-		public static IEnumerable<ApiResource> GetAPIResource()
-		{
-			return new List<ApiResource>()
-			{
-				new ApiResource("resource_api1")
-				{ Scopes =
-					{
-						"api1.read",
-						"api1.write",
-						"api1.update"
-					},
-					ApiSecrets = new[]{new Secret("secretapi1".Sha256())} // passpoword and username for basic auth
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> GetAPIResource()
+        {
+            return new List<ApiResource>()
+            {
+                new ApiResource("resource_api1")
+                { Scopes =
+                    {
+                        "api1.read",
+                        "api1.write",
+                        "api1.update"
+                    },
+                    ApiSecrets = new[]{new Secret("secretapi1".Sha256())} // passpoword and username for basic auth
                 },
-				new ApiResource("resource_api2")
-				{ Scopes =
-					{
-						"api2.read",
-						"api2.write",
-						"api2.update"
-					},
-					ApiSecrets = new[]{new Secret("secretapi2".Sha256())} // passpoword and username for basic auth
+                new ApiResource("resource_api2")
+                { Scopes =
+                    {
+                        "api2.read",
+                        "api2.write",
+                        "api2.update"
+                    },
+                    ApiSecrets = new[]{new Secret("secretapi2".Sha256())} // passpoword and username for basic auth
                 }
-			};
-		}
-		public static IEnumerable<ApiScope> GetAPIScopes()
-		{
-			return new List<ApiScope>()
-			{ new ApiScope("api1.read", "Read permission for api1"),
-			  new ApiScope("api1.write","Write permission for api1"),
-			  new ApiScope("api1.update","Update permission for api1"),
-			  new ApiScope("api2.read", "Read permission for api2"),
-			  new ApiScope("api2.write","Write permission for api2"),
-			  new ApiScope("api2.update","Update permission for api2")
-			};
-		}
-		public static IEnumerable<Client> GetClients()
-		{
-			return new List<Client>()
-			{
-				new Client()
-				{
-					ClientId="client1",
-					ClientName = "Client 1 APP Uygulaması",
+            };
+        }
+        public static IEnumerable<ApiScope> GetAPIScopes()
+        {
+            return new List<ApiScope>()
+            { new ApiScope("api1.read", "Read permission for api1"),
+              new ApiScope("api1.write","Write permission for api1"),
+              new ApiScope("api1.update","Update permission for api1"),
+              new ApiScope("api2.read", "Read permission for api2"),
+              new ApiScope("api2.write","Write permission for api2"),
+              new ApiScope("api2.update","Update permission for api2")
+            };
+        }
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>()
+            {
+                new Client()
+                {
+                    ClientId="client1",
+                    ClientName = "Client 1 APP Uygulaması",
+                    ClientSecrets =new[]
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "api1.read", "api1.write", "api1.update" }
+                },
+                new Client()
+                {
+                    ClientId="client2",
+                    ClientName = "Client 2 APP Uygulaması",
+                    ClientSecrets =new[]
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "api2.read", "api2.write", "api2.update" }
+                },
+                new Client()
+                {
+                    ClientId="Client1-Mvc",
+                    ClientName = "Client1-Mvc APP Uygulaması",
+                    RequirePkce =false, // serverside uygulamalarda gecerli. Secret key Tarayıcıya yüklenmesin diye yapılır. 
 					ClientSecrets =new[]
-					{
-						new Secret("secret".Sha256())
-					},
-					AllowedGrantTypes = GrantTypes.ClientCredentials,
-					AllowedScopes = { "api1.read", "api1.write", "api1.update" }
-				},
-				new Client()
-				{
-					ClientId="client2",
-					ClientName = "Client 2 APP Uygulaması",
-					ClientSecrets =new[]
-					{
-						new Secret("secret".Sha256())
-					},
-					AllowedGrantTypes = GrantTypes.ClientCredentials,
-					AllowedScopes = { "api2.read", "api2.write", "api2.update" }
-				},
-				new Client()
-				{
-					ClientId="Client1-Mvc",
-					ClientName = "Client1-Mvc APP Uygulaması",
-					RequirePkce =false, // serverside uygulamalarda gecerli. Secret key Tarayıcıya yüklenmesin diye yapılır. 
-					ClientSecrets =new[]
-					{
-						new Secret("secret".Sha256())
-					},
-					AllowedGrantTypes = GrantTypes.Hybrid,
-					RedirectUris = new List<string>{ "https://localhost:44375/signin-oidc"}, //Token alma işlemini gerçekleştiren URL'dir.Client1 StartUp da Oidc diye belirttiğimiz için.
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    RedirectUris = new List<string>{ "https://localhost:44375/signin-oidc"}, //Token alma işlemini gerçekleştiren URL'dir.Client1 StartUp da Oidc diye belirttiğimiz için.
 					PostLogoutRedirectUris=new List<string>{ "https://localhost:44375/signout-callback-oidc" },
-					AllowedScopes = {IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile, "api1.read",IdentityServerConstants.StandardScopes.OfflineAccess },//Bu Client hangi izinlere sahip olacak onu belirliyoruz.
+                    AllowedScopes = {IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile, "api1.read",IdentityServerConstants.StandardScopes.OfflineAccess,"CountryAndCity" },//Bu Client hangi izinlere sahip olacak onu belirliyoruz.
 					AccessTokenLifetime=2*60*60,
                     AllowOfflineAccess = true,
-					RefreshTokenUsage=TokenUsage.ReUse,
-					RefreshTokenExpiration = TokenExpiration.Absolute,
-					AbsoluteRefreshTokenLifetime = (int)(DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds, // 60 gün sonra tükenecek.
+                    RefreshTokenUsage=TokenUsage.ReUse,
+                    RefreshTokenExpiration = TokenExpiration.Absolute,
+                    AbsoluteRefreshTokenLifetime = (int)(DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds, // 60 gün sonra tükenecek.
 					RequireConsent=true  // onay ekranı xxx uygulaması senden xxx bilgilerini istiyor vs.
 				}
 
-			};
-		}
+            };
+        }
 
-		public static IEnumerable<IdentityResource> GetIdentityResources()
-		{
-			return new List<IdentityResource>()
-			{
-				new IdentityResources.OpenId() //subId. Gereklidir.
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>()
+            {
+                new IdentityResources.OpenId() //subId. Gereklidir.
 				{
 
-				},
-				new IdentityResources.Profile() // claim bilgilerini tutar
+                },
+                new IdentityResources.Profile() // claim bilgilerini tutar
 				{
 
-				}
-			};
+                },
+                new IdentityResource()
+                {
+                    Name="CountryAndCity", DisplayName="County and City",Description="Kullanıcının ülke ve şehir bilgisi",UserClaims = new[]{"country","city"}
+                }
+            };
 
-		}
-		public static IEnumerable<TestUser> GetUsers()
-		{
-			return new List<TestUser>()
-			{
-				new TestUser()// username email olarak kullanılabilir
+        }
+        public static IEnumerable<TestUser> GetUsers()
+        {
+            return new List<TestUser>()
+            {
+                new TestUser()// username email olarak kullanılabilir
 				{
-					SubjectId = "1",Username ="Nusret",Password = "12345",Claims = new List<Claim>
-					{
-						new Claim("given_name","Nusret"),
-						new Claim("family_name","Simsek"),
-					}
-				},
+                    SubjectId = "1",Username ="Nusret",Password = "12345",Claims = new List<Claim>
+                    {
+                        new Claim("given_name","Nusret"),
+                        new Claim("family_name","Simsek"),
+                        new Claim("country","Türkiye"),
+                        new Claim("city","Istanbul"),
+                    }
+                },
 
-				new TestUser()// username email olarak kullanılabilir
+                new TestUser()// username email olarak kullanılabilir
 				{
-					SubjectId = "2",Username ="Dilek",Password = "11111",Claims = new List<Claim>
-					{
-						new Claim("given_name","Dilek"),
-						new Claim("family_name","Simsek"),
-					}
-				}
+                    SubjectId = "2",Username ="Dilek",Password = "11111",Claims = new List<Claim>
+                    {
+                        new Claim("given_name","Dilek"),
+                        new Claim("family_name","Simsek"),
+                        new Claim("country","Türkiye"),
+                        new Claim("city","Rize"),
 
-			};
-		}
-	}
+                    }
+                }
+
+            };
+        }
+    }
 }
--- IdentityServer.Client1/Program.cs
+++ IdentityServer.Client1/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -19,6 +21,11 @@ builder.Services.AddAuthentication(opts =>
     opts.SaveTokens = true; // access tokenlarý kaydeder.
     opts.Scope.Add("api1.read");// api.read scope'unu da ver bana.
     opts.Scope.Add("offline_access");// RefreshToken istiyoruz.
+    opts.Scope.Add("CountryAndCity");// manuel eklediðimiz scope u istiyoruz.
+
+    opts.ClaimActions.MapUniqueJsonKey("country", "country");
+    opts.ClaimActions.MapUniqueJsonKey("city", "city");
+
 });
 var app = builder.Build();
 
