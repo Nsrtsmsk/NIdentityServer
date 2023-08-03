f077ddab531ee6faf08d968ccf8ad32a3d81f828
Nusret Şimşek <nsimsek@mngkargo.com.tr>
2023-08-03 15:32:55 +0300

    Migration

--- IdentityServer.Auth/Program.cs
+++ IdentityServer.Auth/Program.cs
@@ -1,16 +1,44 @@
 using IdentityServer.Auth;
+using IdentityServer.Auth.Seeds;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// **********entityframework implementation.************
+var assembly = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
+
+//***********************
+//builder.Services.AddIdentityServer()
+//    .AddInMemoryApiResources(Config.GetAPIResource())
+//    .AddInMemoryApiScopes(Config.GetAPIScopes())
+//    .AddInMemoryClients(Config.GetClients())
+//    .AddTestUsers(Config.GetUsers().ToList())
+//    .AddInMemoryIdentityResources(Config.GetIdentityResources())
+//    .AddDeveloperSigningCredential();
 builder.Services.AddIdentityServer()
-    .AddInMemoryApiResources(Config.GetAPIResource())
-    .AddInMemoryApiScopes(Config.GetAPIScopes())
-    .AddInMemoryClients(Config.GetClients())
-    .AddTestUsers(Config.GetUsers().ToList())
-    .AddInMemoryIdentityResources(Config.GetIdentityResources())
-    .AddDeveloperSigningCredential();
+    .AddConfigurationStore(opts =>
+    {
+        var connectionString = builder.Configuration["LocalDb:myDb1"];
+        opts.ConfigureDbContext = c => c.UseSqlServer(connectionString, sqlopt => sqlopt.MigrationsAssembly(assembly));
+        
+    })
+    .AddOperationalStore
+    (
+    opts =>
+    {
+        var connectionString = builder.Configuration["LocalDb:myDb1"];
+        opts.ConfigureDbContext = c => c.UseSqlServer(connectionString, sqlopt => sqlopt.MigrationsAssembly(assembly));
+
+    }
+    );
+    
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,9 +54,14 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseIdentityServer();
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+    using (var serviceScope = app.Services.CreateScope())
+    {
+        var servis = serviceScope.ServiceProvider;
+        var context = servis.GetRequiredService<ConfigurationDbContext>();
+        SeedData.Seed(context);
+    }
 
-app.Run();
+    app.Run();
---
+++ IdentityServer.Auth/Seeds/SeedData.cs
@@ -0,0 +1,43 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+
+namespace IdentityServer.Auth.Seeds
+{
+    public static class SeedData
+    {
+        public static void Seed(ConfigurationDbContext context)
+        { 
+            if (!context.Clients.Any())
+            {
+                foreach (var client in Config.GetClients()) 
+                {
+                    context.Clients.Add(client.ToEntity());
+                
+                }
+            }
+            if (!context.ApiResources.Any()) 
+            {
+                foreach (var apiresource in Config.GetAPIResource())
+                {
+                    context.ApiResources.Add(apiresource.ToEntity());
+                }
+            }
+            if (!context.ApiScopes.Any())
+            {
+                Config.GetAPIScopes().ToList().ForEach(s => 
+                { 
+                    context.ApiScopes.Add(s.ToEntity()); 
+                });
+            }
+            if (!context.IdentityResources.Any())
+            {
+                Config.GetIdentityResources().ToList().ForEach(s =>
+                {
+                    context.IdentityResources.Add(s.ToEntity());
+                });
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
